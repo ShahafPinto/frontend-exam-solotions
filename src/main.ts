bbc9377ed0bda1b7b80eae0a232b5ba07076a57d
ShahafPinto <shahafchen7@gmail.com>
2022-12-04 10:13:36 +0200

    question 1.1.6

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,9 @@
 import './style.scss'
 // keep the line above and write your code below.
+document.addEventListener("DOMContentLoaded",()=>{
+    main();
+})
+
+function main(){
+    // alert('hi')
+}
